1bfc6f24178694e335aa573e0c0cc74f0a54c8ae
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-17 16:01:09 -0400

    Define the TicTacToe class in a separate tic_tac_toe.rb file

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,25 @@
 class TicTacToe
-    attr_accessor :round, :score, :p1,:p2
-    def initialize(p1,p2)
-        self.p1=p1
-        self.p2=p2
+  attr_accessor :round, :score, :p1, :p2, :board
+
+  def initialize(p1, p2, board)
+    self.p1 = p1
+    self.p2 = p2
+    self.board = board
+  end
+
+  def play
+    active_player = p1
+    dormant_player = p2
+    until board.is_full? || board.has_winner?
+      active_player, dormant_player = dormant_player, active_player
+      active_player.make_move(board)
     end
-    def play()
-
+    if board.has_winner?
+      puts "#{board.winner} has won!!"
+    else
+      puts "Good job to your both. It's a draw."
     end
 
-
-    def end_game()
-
-    end
-    def check_win()
-
-    end
+    board.print_board
+  end
 end
