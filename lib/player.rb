775b6c9017b52054ff720ddeb623b0ac5c11a0b8
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-17 16:00:24 -0400

    Define the Player class in a separate player.rb file

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,35 @@
 class Player
-    attr_accessor:name,:value
-    def initialize(name, value)
-        self.name=name
-        self.value=value
+  attr_accessor :name, :value
+
+  def initialize(name, value)
+    self.name = name
+    self.value = value
+  end
+
+  def make_move(board)
+    puts "Select a position #{name}:"
+    valid_value = false
+    until valid_value == true
+      position = gets.chomp
+
+      unless position == position.to_i.to_s
+        puts 'Enter a number between 0-8'
+        next
+      end
+
+      position = position.to_i
+      unless position >= 0 && position < 9
+        puts 'Enter a number between 0-8'
+        next
+      end
+
+      unless board.board[position] == '-'
+        puts "Position #{position} is already taken. Pick another one"
+        next
+      end
+
+      valid_value = true
+      board.set_value(position, value)
     end
-    def make_move(board,position)
-        board.set_value(position,value)
-    end
+  end
 end
